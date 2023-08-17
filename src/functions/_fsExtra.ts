48017a049cfb07bbf578556a4c03198a6a26e4cf
kiki-kanri <a470666@gmail.com>
2023-08-18 02:38:29 +0800

    Edited _fsExtra export and wrong wrapper usage.

--- src/functions/_fsExtra.ts
+++ src/functions/_fsExtra.ts
@@ -14,10 +14,16 @@ export const emptyDirSync = tryAndGetBoolean(fse.emptyDirSync);
 export const ensureFile = tryAndGetBoolean(async (file: string) => await fse.ensureFile(file));
 export const ensureFileSync = tryAndGetBoolean(fse.ensureFileSync);
 
-// ensureDir
+// ensureDir, mkdirp, mkdirs
 type EnsureDirParameters = Parameters<typeof fse.ensureDirSync>;
 export const ensureDir = tryAndGetBoolean(async (...args: EnsureDirParameters) => await fse.ensureDir(...args));
 export const ensureDirSync = tryAndGetBoolean(fse.ensureDirSync);
+
+export const mkdirp = ensureDir;
+export const mkdirpSync = ensureDirSync;
+
+export const mkdirs = ensureDir;
+export const mkdirsSync = ensureDirSync;
 
 // ensureLink
 export const ensureLink = tryAndGetBoolean(async (...args: Parameters<typeof fse.ensureLinkSync>) => await fse.ensureLink(...args));
@@ -27,27 +33,16 @@ export const ensureLinkSync = tryAndGetBoolean(fse.ensureLinkSync);
 export const ensureSymlink = tryAndGetBoolean(async (...args: Parameters<typeof fse.ensureSymlinkSync>) => await fse.ensureSymlink(...args));
 export const ensureSymlinkSync = tryAndGetBoolean(fse.ensureSymlinkSync);
 
-// mkdirp
-export const mkdirp = tryAndGetBoolean(async (...args: EnsureDirParameters) => await fse.mkdirp(...args));
-export const mkdirpSync = tryAndGetBoolean(fse.mkdirpSync);
-
-// mkdirs
-export const mkdirs = tryAndGetBoolean(async (...args: EnsureDirParameters) => await fse.mkdirs(...args));
-export const mkdirsSync = tryAndGetBoolean(fse.mkdirsSync);
-
 // move
-type MoveParameters = Parameters<typeof fse.moveSync>;
-export const move = tryAndGetBoolean(async (...args: MoveParameters) => await fse.move(...args));
+export const move = tryAndGetBoolean(async (...args: Parameters<typeof fse.moveSync>) => await fse.move(...args));
 export const moveSync = tryAndGetBoolean(fse.moveSync);
 
 // outputFile
-type OutputFileParameters = Parameters<typeof fse.outputFileSync>;
-export const outputFile = tryAndGetBoolean(async (...args: OutputFileParameters) => await fse.outputFile(...args));
+export const outputFile = tryAndGetBoolean(async (...args: Parameters<typeof fse.outputFileSync>) => await fse.outputFile(...args));
 export const outputFileSync = tryAndGetBoolean(fse.outputFileSync);
 
 // outputJson
-type OutputJsonParameters = Parameters<typeof fse.outputJsonSync>;
-export const outputJson = tryAndGetBoolean(async (...args: OutputJsonParameters) => await fse.outputJson(...args));
+export const outputJson = tryAndGetBoolean(async (...args: Parameters<typeof fse.outputJsonSync>) => await fse.outputJson(...args));
 export const outputJsonSync = tryAndGetBoolean(fse.outputJsonSync);
 
 // pathExists
@@ -60,10 +55,9 @@ export const readJson = tryAndGetData(async (...args: ReadJsonParameters) => awa
 export const readJsonSync = tryAndGetData(fse.readJsonSync);
 
 // remove
-export const remove = tryAndGetData(async (dir: string) => await fse.remove(dir));
-export const removeSync = tryAndGetData(fse.removeSync);
+export const remove = tryAndGetBoolean(async (dir: string) => await fse.remove(dir));
+export const removeSync = tryAndGetBoolean(fse.removeSync);
 
 // readJson
-type WriteJsonParameters = Parameters<typeof fse.writeJsonSync>;
-export const writeJson = tryAndGetData(async (...args: WriteJsonParameters) => await fse.writeJson(...args));
-export const writeJsonSync = tryAndGetData(fse.writeJsonSync);
+export const writeJson = tryAndGetBoolean(async (...args: Parameters<typeof fse.writeJsonSync>) => await fse.writeJson(...args));
+export const writeJsonSync = tryAndGetBoolean(fse.writeJsonSync);

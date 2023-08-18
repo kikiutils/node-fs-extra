b87c5db41a7bd581896d57b307c67be448674863
kiki-kanri <a470666@gmail.com>
2023-08-18 19:28:05 +0800

    Edit fs and fs/promises files.

--- src/functions/_fs/promises.ts
+++ src/functions/_fs/promises.ts
@@ -1,4 +1,5 @@
 import { tryAndGetBoolean, tryAndGetData } from '@kikiutils/wrappers';
+import fs from 'fs';
 import fsp from 'fs/promises';
 
 // Get boolean
@@ -6,9 +7,18 @@ export const access = tryAndGetBoolean(fsp.access);
 export const appendFile = tryAndGetBoolean(fsp.appendFile);
 export const chmod = tryAndGetBoolean(fsp.chmod);
 export const chown = tryAndGetBoolean(fsp.chown);
+export const close = tryAndGetBoolean(fs.close.__promisify__);
 export const copyFile = tryAndGetBoolean(fsp.copyFile);
+export const cp = tryAndGetBoolean(fsp.cp);
+export const fchmod = tryAndGetBoolean(fs.fchmod.__promisify__);
+export const fchown = tryAndGetBoolean(fs.fchown.__promisify__);
+export const fdatasync = tryAndGetBoolean(fs.fdatasync.__promisify__);
+export const fsync = tryAndGetBoolean(fs.fsync.__promisify__);
+export const ftruncate = tryAndGetBoolean(fs.ftruncate.__promisify__);
+export const futimes = tryAndGetBoolean(fs.futimes.__promisify__);
 export const lchown = tryAndGetBoolean(fsp.lchown);
 export const link = tryAndGetBoolean(fsp.link);
+export const lutimes = tryAndGetBoolean(fsp.lutimes);
 export const rename = tryAndGetBoolean(fsp.rename);
 export const rm = tryAndGetBoolean(fsp.rm);
 export const rmdir = tryAndGetBoolean(fsp.rmdir);
@@ -21,13 +31,22 @@ export const writeFile = tryAndGetBoolean(fsp.writeFile);
 // Get data
 export const open = tryAndGetData(fsp.open);
 export const opendir = tryAndGetData(fsp.opendir);
+export const readv = tryAndGetData(fs.readv.__promisify__);
+export const writev = tryAndGetData(fs.writev.__promisify__);
+
+// Original functions
+export { watch } from 'fs/promises';
 
 // TODO: overloads
+export const fstat = tryAndGetData(fs.fstat.__promisify__);
 export const lstat = tryAndGetData(fsp.lstat);
 export const mkdir = tryAndGetData(fsp.mkdir);
 export const mkdtemp = tryAndGetData(fsp.mkdtemp);
-export const readdir = tryAndGetData(fsp.readdir);
+export const read = tryAndGetData(fs.read.__promisify__);
 export const readFile = tryAndGetData(fsp.readFile);
+export const readdir = tryAndGetData(fsp.readdir);
 export const readlink = tryAndGetData(fsp.readlink);
 export const realpath = tryAndGetData(fsp.realpath);
 export const stat = tryAndGetData(fsp.stat);
+export const statfs = tryAndGetData(fsp.statfs);
+export const write = tryAndGetData(fs.write.__promisify__);
--- src/functions/_fs/sync.ts
+++ src/functions/_fs/sync.ts
@@ -6,9 +6,18 @@ export const accessSync = tryAndGetBoolean(fs.accessSync);
 export const appendFileSync = tryAndGetBoolean(fs.appendFileSync);
 export const chmodSync = tryAndGetBoolean(fs.chmodSync);
 export const chownSync = tryAndGetBoolean(fs.chownSync);
+export const closeSync = tryAndGetBoolean(fs.closeSync);
 export const copyFileSync = tryAndGetBoolean(fs.copyFileSync);
+export const cpSync = tryAndGetBoolean(fs.cpSync);
+export const fchmodSync = tryAndGetBoolean(fs.fchmodSync);
+export const fchownSync = tryAndGetBoolean(fs.fchownSync);
+export const fdatasyncSync = tryAndGetBoolean(fs.fdatasyncSync);
+export const fsyncSync = tryAndGetBoolean(fs.fsyncSync);
+export const ftruncateSync = tryAndGetBoolean(fs.ftruncateSync);
+export const futimesSync = tryAndGetBoolean(fs.futimesSync);
 export const lchownSync = tryAndGetBoolean(fs.lchownSync);
 export const linkSync = tryAndGetBoolean(fs.linkSync);
+export const lutimesSync = tryAndGetBoolean(fs.lutimesSync);
 export const renameSync = tryAndGetBoolean(fs.renameSync);
 export const rmSync = tryAndGetBoolean(fs.rmSync);
 export const rmdirSync = tryAndGetBoolean(fs.rmdirSync);
@@ -21,13 +30,22 @@ export const writeFileSync = tryAndGetBoolean(fs.writeFileSync);
 // Get data
 export const openSync = tryAndGetData(fs.openSync);
 export const opendirSync = tryAndGetData(fs.opendirSync);
+export const readvSync = tryAndGetData(fs.readvSync);
+export const writevSync = tryAndGetData(fs.writevSync);
+
+// Original functions
+export { existsSync } from 'fs';
 
 // TODO: overloads
+export const fstatSync = tryAndGetData(fs.fstatSync);
 export const lstatSync = tryAndGetData(fs.lstatSync);
 export const mkdirSync = tryAndGetData(fs.mkdirSync);
 export const mkdtempSync = tryAndGetData(fs.mkdtempSync);
-export const readdirSync = tryAndGetData(fs.readdirSync);
 export const readFileSync = tryAndGetData(fs.readFileSync);
+export const readSync = tryAndGetData(fs.readSync);
+export const readdirSync = tryAndGetData(fs.readdirSync);
 export const readlinkSync = tryAndGetData(fs.readlinkSync);
 export const realpathSync = tryAndGetData(fs.realpathSync);
 export const statSync = tryAndGetData(fs.statSync);
+export const statfsSync = tryAndGetData(fs.statfsSync);
+export const writeSync = tryAndGetData(fs.writeSync);

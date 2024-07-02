87623fdea3170e5d8b024482b4e3e8f4062f83d6
kiki-kanri <a470666@gmail.com>
2024-07-03 01:16:56 +0800

    feat: return an error-throwing function when undefined function values are received in wrapped function creation

--- src/wrappers.ts
+++ src/wrappers.ts
@@ -1,6 +1,12 @@
-import type { AnyFunction } from './types';
+type AnyFunction = (...args: any[]) => any;
 
-export const tB = <F extends AnyFunction, P extends Parameters<F>, RT extends ReturnType<F> extends Promise<any> ? Promise<boolean> : boolean>(toWrapFunction: F): ((...args: P) => RT) => {
+export const tB = <F extends AnyFunction, P extends Parameters<F>, RT extends ReturnType<F> extends Promise<any> ? Promise<boolean> : boolean>(toWrapFunction?: F): ((...args: P) => RT) => {
+	if (!toWrapFunction) {
+		return () => {
+			throw new Error('The funciton passed to the wrapper is not realized, please check if the nodejs/deno/bun version supports this function.');
+		};
+	}
+
 	return function (...args: P) {
 		try {
 			const result = toWrapFunction(...args);
@@ -11,7 +17,13 @@ export const tB = <F extends AnyFunction, P extends Parameters<F>, RT extends Re
 	};
 };
 
-export const tD = <F extends AnyFunction, P extends Parameters<F>, R extends ReturnType<F>, RT extends R extends Promise<any> ? Promise<Awaited<R> | T> : R | T, T = undefined>(toWrapFunction: F, returnValueOnError?: T): ((...args: P) => RT) => {
+export const tD = <F extends AnyFunction, P extends Parameters<F>, R extends ReturnType<F>, RT extends R extends Promise<any> ? Promise<Awaited<R> | T> : R | T, T = undefined>(toWrapFunction?: F, returnValueOnError?: T): ((...args: P) => RT) => {
+	if (!toWrapFunction) {
+		return () => {
+			throw new Error('The funciton passed to the wrapper is not realized, please check if the nodejs/deno/bun version supports this function.');
+		};
+	}
+
 	return function (...args: P) {
 		try {
 			const result = toWrapFunction(...args);

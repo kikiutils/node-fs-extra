945d7b237d1ab29f64c7e6d820ec1973497b7a60
kiki-kanri <a470666@gmail.com>
2024-07-07 13:48:52 +0800

    fix: ensure entry point exports default

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
+// @ts-ignore
+import type fs from 'node:fs';
+// @ts-ignore
+import type fse from 'fs-extra';
+
+import * as _additional from './additional';
+import * as _fs from './fs';
+import * as _fsExtra from './fsExtra';
+import { ToWrapFunctionIsUndefinedError } from './wrappers';
+
 export * from './additional';
 export * from './fs';
 export * from './fsExtra';
 export { ToWrapFunctionIsUndefinedError } from './wrappers';
+
+export default {
+	..._additional,
+	..._fs,
+	..._fsExtra,
+	ToWrapFunctionIsUndefinedError
+};

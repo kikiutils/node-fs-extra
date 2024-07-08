5cc8b4f1aa0a33f9b00e1f10a6c0df5a5763dfe4
kiki-kanri <a470666@gmail.com>
2024-07-08 15:24:56 +0800

    refactor: modify entry point and method of exporting all functions

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,2 @@
-// @ts-ignore
-import type fs from 'node:fs';
-// @ts-ignore
-import type fse from 'fs-extra';
-
-import * as _additional from './additional';
-import * as _fs from './fs';
-import * as _fsExtra from './fsExtra';
-import { ToWrapFunctionIsUndefinedError } from './wrappers';
-
-export * from './additional';
-export * from './fs';
-export * from './fsExtra';
-export { ToWrapFunctionIsUndefinedError } from './wrappers';
-
-export default {
-	..._additional,
-	..._fs,
-	..._fsExtra,
-	ToWrapFunctionIsUndefinedError
-};
+export * as default from './_index';
+export * from './_index';

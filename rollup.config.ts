059619c1204ca580885aca09cb97aad6f391bbbd
kiki-kanri <a470666@gmail.com>
2024-07-21 23:55:34 +0800

    build: add ESM format files to build output

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,35 +1,32 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import { defineConfig } from 'rollup';
+import type { OutputOptions } from 'rollup';
 import del from 'rollup-plugin-delete';
 import nodeExternals from 'rollup-plugin-node-externals';
 
+const generateOutputOptions = (format: 'cjs' | 'esm'): OutputOptions => ({
+	dir: './dist',
+	entryFileNames: `[name].${format === 'cjs' ? 'cjs' : 'mjs'}`,
+	externalLiveBindings: false,
+	format,
+	generatedCode: {
+		arrowFunctions: true,
+		constBindings: true,
+		objectShorthand: true
+	},
+	interop: 'compat',
+	preserveModules: true,
+	preserveModulesRoot: './src'
+});
+
 export default defineConfig({
 	input: './src/index.ts',
-	output: {
-		dir: './dist',
-		externalLiveBindings: false,
-		format: 'cjs',
-		generatedCode: {
-			arrowFunctions: true,
-			constBindings: true,
-			objectShorthand: true
-		},
-		interop: 'compat',
-		preserveModules: true,
-		preserveModulesRoot: './src'
-	},
+	output: [generateOutputOptions('cjs'), generateOutputOptions('esm')],
 	plugins: [
 		del({ targets: './dist' }),
-		nodeExternals({
-			include: [
-				'hono/hono-base',
-				'hono/logger',
-				'hono/types'
-			]
-		}),
+		nodeExternals(),
 		nodeResolve(),
 		typescript()
-
 	]
 });
